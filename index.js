3e5a40a31f3dd9e93c34fc46ae983932e03655c4
krju13 <krju13@gmail.com>
2021-06-28 00:17:39 +0900

    except password

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')  //express mode get
 const app = express() //app make
 const port = 5000  // port number
+const bodyParser=require('body-parser');
 
+const config=require('./config/key');
+const {User}=require("./models/User");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://chuchu:chu123@utubeclone.wo9of.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true,useFindAndModify:false
 }).then(()=> console.log('MongoDB Connected...')).catch(err=> console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!  HI H,I')
+  res.send('Hello World!  HI H,I, 안녕하세요!')
+})
+
+app.post('/register',(req,res)=>{
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user=new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+
 })
 
 app.listen(port, () => {
